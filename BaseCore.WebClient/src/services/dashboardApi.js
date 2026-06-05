a7d532b8a26e56edd6a05ccc687ce4ed6851f2f5
Datdz <phamthanhdat.192005@gmail.com>
2026-06-05 23:02:08 +0700

    Stop tracking build artifacts

--- BaseCore.WebClient/src/services/dashboardApi.js
+++
@@ -1,8 +0,0 @@
-import { apiClient } from './httpClient';
-
-export const dashboardApi = {
-  async stats() {
-    const response = await apiClient.get('/api/dashboard/stats');
-    return response.data;
-  },
-};
